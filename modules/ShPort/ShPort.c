c0daba25a534731882dbc0a06bf1715bf4b99ee2
kichikuou <KichikuouChrome@gmail.com>
2021-01-24 10:33:38 +0900

    Implement ShPort.OutputMessageBox

--- modules/ShPort/ShPort.c
+++ modules/ShPort/ShPort.c
@@ -36,6 +36,7 @@
 #include "xsystem35.h"
 #include "modules.h"
 #include "sdl_core.h"
+#include "sdl_private.h"
 #include "menu.h"
 
 // キー変換テーブル
@@ -45,12 +46,20 @@ static BYTE *keymap[KEYMAP_MAX];
 static void OutputMessageBox(void) { /* 0 */
 	int p1 = getCaliValue();
 	int p2 = getCaliValue();
-	int p3 = getCaliValue();
-	int p4 = getCaliValue();
-	int p5 = getCaliValue();
-	int p6 = getCaliValue();
+	int title = getCaliValue();
+	int msg = getCaliValue();
+	int *res = getCaliVariable();
+	int ISys3xSystem = getCaliValue();
 
-	DEBUG_COMMAND_YET("ShPort.OutputMessageBox: %d,%d,%d,%d,%d,%d:\n", p1, p2, p3, p3, p4, p5, p6);
+	char *title_utf8 = toUTF8(svar_get(title));
+	char *msg_utf8 = toUTF8(svar_get(msg));
+
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, title_utf8, msg_utf8, sdl_window);
+
+	free(title_utf8);
+	free(msg_utf8);
+
+	DEBUG_COMMAND("ShPort.OutputMessageBox: %d,%d,%d,%d,%p,%d:\n", p1, p2, title, msg, res, ISys3xSystem);
 }
 
 static void InputListNum(void) { /* 1 */
