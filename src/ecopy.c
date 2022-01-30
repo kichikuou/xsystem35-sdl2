83989f1a0e55c6ac846934c8e043bd02e474f022
kichikuou <KichikuouChrome@gmail.com>
2022-01-30 15:31:20 +0900

    CE command: Fix sdl_effect parameters when WV origin is not (0,0)

--- src/ecopy.c
+++ src/ecopy.c
@@ -858,7 +858,7 @@ void ags_eCopyArea(int sx, int sy, int w, int h, int dx, int dy, int sw, int opt
 
 	enum sdl_effect_type sdl_effect = from_nact_effect(sw);
 	if (sdl_effect != EFFECT_INVALID) {
-		SDL_Rect rect = { dx, dy, w, h };
+		SDL_Rect rect = { dx - nact->sys_view_area.x, dy - nact->sys_view_area.y, w, h };
 		struct sdl_effect *eff = sdl_effect_init(&rect, sdl_getDIB(), dx, dy, sdl_getDIB(), sx, sy, from_nact_effect(sw));
 		ags_fade2(duration(sw, opt, &rect), cancel, (ags_fade2_callback)sdl_effect_step, eff);
 		sdl_effect_finish(eff);
