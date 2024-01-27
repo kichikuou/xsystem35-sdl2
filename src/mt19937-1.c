7b1fef4ad703a77b75ce5e8dd3685f884fed1e18
kichikuou <KichikuouChrome@gmail.com>
2024-01-27 09:08:31 +0900

    Fix a deprecated-non-prototype warning

--- src/mt19937-1.c
+++ src/mt19937-1.c
@@ -54,9 +54,7 @@ static unsigned long mt[N]; /* the array for the state vector  */
 static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
 
 /* initializing the array with a NONZERO seed */
-void
-sgenrand(seed)
-    unsigned long seed;	
+void sgenrand(unsigned long seed)
 {
     /* setting initial seeds to mt[N] using         */
     /* the generator Line 25 of Table 1 in          */
