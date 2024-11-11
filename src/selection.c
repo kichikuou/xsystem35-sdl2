kichikuou/xsystem35-sdl2

commit eeeab6e05472c96dd55fddf592ea9cfd4bf829a8

Right-click exits selection without waiting for button up
This fixes https://github.com/kichikuou/xsystem35-sdl2/issues/56.

src/selection.c
@@ -460,7 +460,9 @@ void sel_select() {
 	while (!nact->is_quit) {
 		key = sys_keywait(25, KEYWAIT_CANCELABLE);
 
-		if (!key && prevkey == SYS35KEY_SPC) break;
+		// Right-click exits the menu without waiting for button up.
+		if (key == SYS35KEY_SPC) break;
+
 		if (!key && prevkey == SYS35KEY_RET && curElement != -1) break;
 		prevkey = key;
 

commit 36e1dc152ba04e4ca295d558cded1de31da3be30

Fix crash when canceling menu with two-finger touch
Fixes #58.

It was a regression by commit eeeab6e.

src/selection.c
@@ -461,7 +461,10 @@ void sel_select() {
 		key = sys_keywait(25, KEYWAIT_CANCELABLE);
 
 		// Right-click exits the menu without waiting for button up.
-		if (key == SYS35KEY_SPC) break;
+		if (key == SYS35KEY_SPC) {
+			curElement = -1;
+			break;
+		}
 
 		if (!key && prevkey == SYS35KEY_RET && curElement != -1) break;
 		prevkey = key;
@@ -536,7 +539,7 @@ void sel_select() {
 		msg_nextPage(TRUE);
 	}
 	
-	if (prevkey == SYS35KEY_RET) {
+	if (curElement != -1) {
 		sl_jmpNear(elmv[curElement]);
 		if (cb_select_page != 0) {
 			sl_callFar2(cb_select_page -1, cb_select_address);